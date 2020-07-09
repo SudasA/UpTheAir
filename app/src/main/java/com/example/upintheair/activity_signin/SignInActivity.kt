80b71f8f81147c7a4c35ea9226890e48e758d306
MsLeoHelen <ms.leohelen@gmail.com>
2020-07-09 19:21:57 +0300

    feature: add sign in activity (UI)

---
+++ app/src/main/java/com/example/upintheair/activity_signin/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.upintheair.activity_signin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.upintheair.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}

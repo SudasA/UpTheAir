bb8c70876281eca2efacb92a530c0bd6ace038ab
MsLeoHelen <ms.leohelen@gmail.com>
2020-07-10 10:54:56 +0300

    feature: add post request for create user

---
+++ app/src/main/java/com/example/upintheair/network/RetrofitRepository.kt
@@ -0,0 +1,39 @@
+package com.example.upintheair.network
+
+import com.example.upintheair.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class RetrofitRepository() {
+
+    private lateinit var userService: UserService
+
+    fun getUserService(): UserService {
+        if (!::userService.isInitialized) {
+            val retrofit = Retrofit.Builder()
+//                .baseUrl(BuildConfig.BASE_URL)
+                .baseUrl("https://upintheair.getsandbox.com:443")
+                .client(getClient())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            userService = retrofit.create(UserService::class.java)
+        }
+
+        return userService
+    }
+
+    private fun getClient(): OkHttpClient {
+        val logger = HttpLoggingInterceptor()
+        logger.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .callTimeout(30, TimeUnit.SECONDS)
+
+        return client.build()
+    }
+}

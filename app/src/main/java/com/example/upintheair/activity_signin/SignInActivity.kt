96262fb80df17838077dbb37aa0ff62090e3e20d
MsLeoHelen <ms.leohelen@gmail.com>
2020-07-17 18:04:12 +0300

    fix: mvvm, observer, coroutines

--- app/src/main/java/com/example/upintheair/activity_signin/SignInActivity.kt
+++ app/src/main/java/com/example/upintheair/activity_signin/SignInActivity.kt
@@ -5,13 +5,8 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.upintheair.R
-import com.example.upintheair.entity.UserRequest
 import com.example.upintheair.loginFilter
-import com.example.upintheair.md5
 import kotlinx.android.synthetic.main.activity_signin.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SignInActivity : AppCompatActivity() {
@@ -25,35 +20,33 @@ class SignInActivity : AppCompatActivity() {
         login.filters += loginFilter()
 
         buttonSingIn.setOnClickListener(clickOnButtonSignIn)
+        textLogIn.setOnClickListener {
+            openLogIn()
+        }
     }
 
-    val clickOnButtonSignIn = object : View.OnClickListener {
+    private val clickOnButtonSignIn = object : View.OnClickListener {
         override fun onClick(v: View?) {
+            mViewModel.sendUser(
+                login.text.toString(),
+                username.text.toString(),
+                password.text.toString(),
+                repeatPassword.text.toString()
+            )
 
-            if (login.text.toString() != "" && username.text.toString() != "" && password.text.toString() != "" && repeatPassword.text.toString() != "") {
-                if (checkPassword(password.text.toString(), repeatPassword.text.toString())) {
-//                    CoroutineScope(Dispatchers.Main).launch {
-                        mViewModel.postUser(
-                            UserRequest(
-                                login.text.toString(),
-                                username.text.toString(),
-                                password.text.toString().md5()
-                            )
-                        )
-//                    }
-                } else {
-                    errorText.text = "Вы неправильно повторили пароль"
-                }
+            if (mViewModel.errorLiveData.value != null){
+                errorText.text = mViewModel.errorLiveData.value
             } else {
-                errorText.text = "Все поля должны быть заполнены"
+                openLogIn()
             }
         }
+    }
+
+    fun openLogIn() {
+
     }
 
     fun toastMessage(message: String) {
         Toast.makeText(this@SignInActivity, message, Toast.LENGTH_SHORT).show()
     }
-
-    fun checkPassword(p1: String, p2: String): Boolean = p1 == p2
-
 }
--- app/src/main/java/com/example/upintheair/activity_signin/SignInViewModel.kt
+++ app/src/main/java/com/example/upintheair/activity_signin/SignInViewModel.kt
@@ -1,30 +1,48 @@
 package com.example.upintheair.activity_signin
 
-import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.upintheair.entity.UserRequest
 import com.example.upintheair.network.RetrofitRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import okhttp3.ResponseBody
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.async
 import kotlin.coroutines.CoroutineContext
 
 class SignInViewModel(
     private val repository: RetrofitRepository
 ) : ViewModel(), CoroutineScope {
+//    private val coroutineScope = CoroutineScope(Dispatchers.IO)
 
+    var errorLiveData = MutableLiveData<String>()
+    var tempLiveData = MutableLiveData<String>()
 
-
-    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+    fun sendUser(
+        login: String,
+        username: String,
+        password: String,
+        repeatPassword: String
+    ) {
+        if (login != "" && username != "" && password != "" && repeatPassword != "") {
+            if (checkPassword(password, repeatPassword)) {
+                CoroutineScope(coroutineContext).async {
+                    val user = UserRequest(login, username, password)
+                    postUser(user)
+                }
+            } else {
+                errorLiveData.value = "Вы неправильно повторили пароль"
+            }
+        } else {
+            errorLiveData.value = "Все поля должны быть заполнены"
+        }
+    }
 
     suspend fun postUser(user: UserRequest) {
-        repository.getUserService().postUser(user)
+        repository.getUserService().postUser(user).string()
     }
 
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main
 
+    fun checkPassword(p1: String, p2: String): Boolean = p1 == p2
 }
